6872b330b1ea832398a3ed804b6e8b18474b83c8
Michel Domingos <micheldslive@gmail.com>
2024-07-01 13:48:23 -0300

    chore: update hero-level.spec.js

--- hero-level.spec.js
+++ hero-level.spec.js
@@ -10,19 +10,19 @@ describe("HeroLevel", () => {
   it(`should return "${label.bronze}" when between 1.001 ~ 2.000`, () => {
     expect(hero.get(1500)).toEqual(label.bronze);
   });
-  it(`should return "${label.prata}" when between 2.001 e 5.000`, () => {
+  it(`should return "${label.prata}" when between 2.001 ~ 5.000`, () => {
     expect(hero.get(3000)).toEqual(label.prata);
   });
-  it(`should return "${label.ouro}" when between 5.001 e 7.000`, () => {
+  it(`should return "${label.ouro}" when between 5.001 ~ 7.000`, () => {
     expect(hero.get(6000)).toEqual(label.ouro);
   });
-  it(`should return "${label.platina}" when between 7.001 e 8.000`, () => {
+  it(`should return "${label.platina}" when between 7.001 ~ 8.000`, () => {
     expect(hero.get(7500)).toEqual(label.platina);
   });
-  it(`should return "${label.ascendente}" when between 8.001 e 9.000`, () => {
+  it(`should return "${label.ascendente}" when between 8.001 ~ 9.000`, () => {
     expect(hero.get(8600)).toEqual(label.ascendente);
   });
-  it(`should return "${label.imortal}" when between 9.001 e 10.000`, () => {
+  it(`should return "${label.imortal}" when between 9.001 ~ 10.000`, () => {
     expect(hero.get(9999)).toEqual(label.imortal);
   });
   it(`should return "${label.radiante}" when greater than or equal 10.001`, () => {
